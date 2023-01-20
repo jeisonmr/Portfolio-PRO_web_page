b0933ad713a9c01af0918295f48145d501806815
jeisonmr <jeisonmzr@gmail.com>
2023-01-19 22:13:35 -0500

    New update

---
+++ src/components/json/Menu.js
@@ -0,0 +1,20 @@
+const lista = [
+  {
+    item: "Inicio",
+    url: "#home",
+  },
+  {
+    item: "Conoceme",
+    url: "#about",
+  },
+  {
+    item: "Profesional",
+    url: "#skill",
+  },
+  {
+    item: "Portafolio",
+    url: "#project",
+  },
+];
+
+export default lista;
