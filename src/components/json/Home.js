0278f299e0aabce19dfafc62f07ce7619b2f93b7
jeisonmr <jeisonmzr@gmail.com>
2023-01-24 15:24:43 -0500

    nivel medio del Home

---
+++ src/components/json/Home.js
@@ -0,0 +1,17 @@
+const dataHome =  {
+            title: "JEISON",
+            subtitle: "MUÑOZ",
+            description: [
+                  "Front end developer",
+                  // "WordPress developer"
+            ],
+            social: [
+                  "/public/linkedin.svg",
+                  "/public/github.svg",
+                  "/public/cv.svg",
+                  "/public/qr.webp"
+            ],
+            video : "../../Resource/vid/vBannerDesktop.webm"
+}
+
+export default dataHome;
