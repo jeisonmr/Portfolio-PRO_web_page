6c64bdcaf38fbe936e00c1d6ee548f147eee9202
jeisonmr <jeisonmzr@gmail.com>
2023-01-28 16:34:00 -0500

    Section about completed

--- src/components/json/Home.js
+++ src/components/json/Home.js
@@ -8,19 +8,19 @@ const dataHome = {
   social: [
     {
       url: "https://bit.ly/linkedin-jeisonmr",
-      img: "/public/linkedin.webp",
+      img: "/public/Resource/img/logos/linkedin.webp",
     },
     {
       url: "https://bit.ly/GitHub-jeisonmr",
-      img: "/public/github.webp",
+      img: "/public/Resource/img/logos/github.webp",
     },
     {
       url: "https://bit.ly/cvJeison",
-      img: "/public/cv.webp",
+      img: "/public/Resource/img/logos/cv.webp",
     },
     {
-      url: "https://bit.ly/Whatsapp-social",
-      img: "/public/qr.webp",
+      url: "https://bit.ly/jeisonmr",
+      img: "/public/Resource/img/logos/qr.webp",
     },
   ],
   video: "../../Resource/vid/vBannerDesktop.webm",
---
+++ src/components/json/Skills.js
@@ -0,0 +1,65 @@
+const dataSkills = {
+  habilidades: {
+    titulo: "habilidades",
+    subtitulo: "Lenguajes",
+    img: [
+      "/public/Resource/img/habilidades/html.svg",
+      "/public/Resource/img/habilidades/css.svg",
+      "/public/Resource/img/habilidades/js.svg",
+      "/public/Resource/img/habilidades/typescript.svg",
+    ],
+    lenaguajes: ["HTML", "CSS", "JavaScript", "TypeScript"],
+  },
+  experiencia: {
+    titulo: "Experiencia",
+    empresas: [
+      "PNG Technology Solutions",
+      "Freelancer",
+      "Canadian PVC",
+      "JV Disingelect",
+      "Populi",
+    ],
+    cargos: [
+      "Developer Front end",
+      "Front end and WordPress",
+      "Development engineer",
+      "WordPress developer",
+      "WordPress developer",
+    ],
+  },
+  educacion: {
+    titulo: "Edicación",
+    certificado: ["Ingeniero de sistemas", "SCRUM Foundation"],
+    entidad: ["Politecnico Costa Atlantica", "CertiProf"],
+  },
+  personales: {
+    titulo: "H. Personales",
+    descripcion: ["Organizado", "Trabajo en equipo", "Profesional"],
+  },
+  hobbies: {
+    titulo: "Hobies",
+    hobbies: [
+      {
+        titulo: "Viajar",
+        imagen: "/src/resource/img/hobbies/viajar.webp",
+      },
+      {
+        titulo: "Cine",
+        imagen: "/src/resource/img/hobbies/cine.webp",
+      },
+      {
+        titulo: "GyM",
+        imagen: "/src/resource/img/hobbies/gym.webp",
+      },
+      {
+        titulo: "Estudio",
+        imagen: "/src/resource/img/hobbies/estudio.webp",
+      },
+    ],
+  },
+  logotipo: {
+    logo: "/src/resource/img/habilidades/jm.webp",
+  },
+};
+
+export default dataSkills;
