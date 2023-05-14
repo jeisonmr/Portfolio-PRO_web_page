279df8e0c7cef89542e96b870b8e4024da4f7ac2
jeisonmr <jeisonmzr@gmail.com>
2023-05-14 00:46:06 -0500

    Cambios visuales

--- src/components/json/Social.js
+++ src/components/json/Social.js
@@ -5,18 +5,22 @@ import qr from "/public/Resource/img/logos/qr.webp";
 const dataSocial = {
       social: [
             {
+              key: 1,
               url: "https://bit.ly/linkedin-jeisonmr",
               img: linkedin,
             },
             {
+              key: 2,
               url: "https://bit.ly/GitHub-jeisonmr",
               img: github,
             },
             {
+              key: 3,
               url: "https://bit.ly/cvJeison",
               img: cv,
             },
             {
+              key: 4,
               url: "https://bit.ly/jeisonmr",
               img: qr,
             },
