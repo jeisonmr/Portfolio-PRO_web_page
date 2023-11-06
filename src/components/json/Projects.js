6dca9d7bb1aa94e5195169f936514d97e4d033bb
Jeison Muñoz Rada <jeisonmzr@gmail.com>
2023-11-05 19:51:04 -0500

    Mas cambios en la sección de proyectos

--- src/components/json/Projects.js
+++ src/components/json/Projects.js
@@ -1,63 +1,144 @@
-const andreisleal = "/public/Resource/img/proyectos/andreis.webp";
-const biofood = "/public/Resource/img/proyectos/bio.webp";
-const campusultra = "/public/Resource/img/proyectos/campus.webp";
-const gascaribe = "/public/Resource/img/proyectos/gas.webp";
-const alutraficled = "/public/Resource/img/proyectos/led.webp";
-const png = "/public/Resource/img/proyectos/png.webp";
-const agenciapopuli = "/public/Resource/img/proyectos/populi.webp";
-const ultracem =  "/public/Resource/img/proyectos/ultra.webp";
+const andreisleal = "/public/Resource/img/proyectos/andreis.png";
+const biofood = "/public/Resource/img/proyectos/biofood.png";
+const campusultra = "/public/Resource/img/proyectos/campus-ultra.png";
+const gascaribe = "/public/Resource/img/proyectos/gases-del-caribe.png";
+const alutraficled = "/public/Resource/img/proyectos/alutraficled.png";
+const png = "/public/Resource/img/proyectos/png.png";
+const agenciapopuli = "/public/Resource/img/proyectos/populi.png";
+const ultracem_colombia = "/public/Resource/img/proyectos/ultracem-col.png";
+const ultracem_honduras = "/public/Resource/img/proyectos/ultracem-hon.png";
+const ultracem_guatemala = "/public/Resource/img/proyectos/ultracem-gua.png";
+const ultracem_panama = "/public/Resource/img/proyectos/ultracem-pan.png";
+const codegar = "/public/Resource/img/proyectos/codegar.png";
+const casalianza = "/public/Resource/img/proyectos/casalianza.png";
+const infortec = "/public/Resource/img/proyectos/infortec.png";
+const sneaker = "/public/Resource/img/proyectos/sneaker.png";
+const sole = "/public/Resource/img/proyectos/sole.png";
+const portafolio = "/public/Resource/img/proyectos/portafolio.png";
+const seven = "/public/Resource/img/proyectos/seven-time.png";
+const wtc = "/public/Resource/img/proyectos/wtc.png";
+
 const dataProjects = {
-  titulo: ["P","R","O","Y","E","C","T","O","S"],
+  titulo: ["P", "R", "O", "Y", "E", "C", "T", "O", "S"],
   botones: ["All", "Frameworks", "Vanilla", "WordPress"],
   proyectos: [
     {
-      img : andreisleal,
+      img: andreisleal,
       url: "https://portal.deandreislegal.co/login",
-      proyecto: "Andreis Leal",
-      tecnologias: ["React","Bootstrap","Typescript"]
+      proyecto: "De Andreis",
+      tecnologias: ["React", "Bootstrap", "Typescript"],
     },
     {
-      img : biofood,
+      img: biofood,
       url: "https://biofoodsoftware.com/",
       proyecto: "Biofood",
-      tecnologias:["Vanilla Js","Bootstrap","CSS","Javasript"]
+      tecnologias: ["Vanilla Js", "Bootstrap", "CSS", "Javasript"],
     },
     {
-      img : campusultra,
+      img: campusultra,
       url: "https://campusultra.com/",
       proyecto: "Campus Ultra",
-      tecnologias:["WordPress","Elementor"]
+      tecnologias: ["WordPress", "Elementor"],
     },
     {
-      img : gascaribe,
+      img: gascaribe,
       url: "https://gascaribe.com/",
       proyecto: "Gases del caribe",
-      tecnologias:["WordPress","Divi"]
+      tecnologias: ["WordPress", "Divi"],
     },
     {
-      img : alutraficled,
+      img: alutraficled,
       url: "https://alutraficled.com/",
       proyecto: "AlutraficLed",
-      tecnologias:["WordPress","Elementor"]
+      tecnologias: ["WordPress", "Elementor"],
     },
     {
-      img : png,
+      img: png,
       url: "https://pngtechnology.co/",
-      proyecto: "Biofood",
-      tecnologias:["Vanilla Js","Bootstrap","CSS","Javasript"]
+      proyecto: "PNG Technology Solutions",
+      tecnologias: ["Vanilla Js", "Bootstrap", "CSS", "Javasript"],
     },
     {
-      img : agenciapopuli,
+      img: agenciapopuli,
       url: "https://agenciapopuli.com/",
       proyecto: "ADN Populi",
-      tecnologias:["WordPress","Elementor"]
+      tecnologias: ["WordPress", "Elementor"],
     },
     {
-      img : ultracem,
+      img: codegar,
+      url: "https://codegar.com/",
+      proyecto: "Codegar",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: casalianza,
+      url: "https://www.casalianzacolombia.com/",
+      proyecto: "Casalianza",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: infortec,
+      url: "https://www.infortec.co/",
+      proyecto: "Infortec",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: sneaker,
+      url: "https://sneakergirls.store/",
+      proyecto: "Sneaker Girl",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: sole,
+      url: "https://www.solesoluciones.com/",
+      proyecto: "Solé",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: portafolio,
+      url: "https://jeisondev.pro/",
+      proyecto: "Portafolio",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: seven,
+      url: "https://seven7times.com/",
+      proyecto: "Seven 7imes",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: wtc,
+      url: "https://wtcpereira.com/",
+      proyecto: "World Trade Center",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    {
+      img: ultracem_colombia,
       url: "https://ultracem.co/",
-      proyecto: "Campus Ultra",
-      tecnologias:["WordPress","Elementor"]
-    }
+      proyecto: "Campus Ultra Colombia",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    ,
+    {
+      img: ultracem_honduras,
+      url: "https://ultracem.hn/",
+      proyecto: "Campus Ultra Honduras",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    ,
+    {
+      img: ultracem_guatemala,
+      url: "https://ultracem.gt/",
+      proyecto: "Campus Ultra Guatemala",
+      tecnologias: ["WordPress", "Elementor"],
+    },
+    ,
+    {
+      img: ultracem_panama,
+      url: "https://ultracem.pa/",
+      proyecto: "Campus Ultra Panamá",
+      tecnologias: ["WordPress", "Elementor"],
+    },
   ],
 };
 
